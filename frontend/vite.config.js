794be4cb88d04a05dc309dda65d2167f3598c34b
tuhin1522 <mdtuhin1499@gmail.com>
2026-04-28 13:12:21 +0600

    feat: add backend requirements and update Vite config for environment loading

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { cwd } from 'node:process'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, cwd(), "")
+
+  console.log(env.VITE_DEBUG)
+
+  return {
+    plugins: [react()],
+    server: {
+    ...(env.VITE_DEBUG === "true" && {
+      proxy: {
+        "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+    )}
+  }
 })
